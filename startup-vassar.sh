b8414c91e84cd035e35952f0827f4b59bb9bbb37
Keith Suderman <suderman@anc.org>
2015-10-01 17:46:39 -0400

    Really fixed the vassar startup script.

--- startup-vassar.sh
+++ startup-vassar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 LOG=/usr/share/tomcat/service-manager/logs/catalina.out
 SERVER=http://vassar:8080
 
@@ -11,14 +13,25 @@ done
 service tomcat start
 
 if [ -d /etc/lddl ] ; then
+	until [ -e $LOG ] ; do
+		echo "Waiting for the Tomcat log to appear."
+		sleep 2
+	done
+	
 	# Watch the log file for the startup complete message from Tomcat
-	tail -f $LOG | grep "INFO: Server startup in" | read -t 300 dummy_var
-	if [ $? -eq 0 ] ; then
-		cd /etc/lddl
-		lddl Vassar.lddl -server=$SERVER
-	else
-		echo "Tomcat did not start in a timely fashion. Aborting."
-	fi
+	
+	# This should work, but doesn't...
+	# tail -f $LOG | grep "INFO: Server startup in" | read -t 300 dummy_var
+	
+	# This is a hack, but it works.
+	started=`cat $LOG | grep "INFO: Server startup in"`
+	while [ -z "$started" ] ; do
+		echo "Waiting for Tomcat to startup."
+		sleep 2
+		started=`cat $LOG | grep "INFO: Server startup in"`
+	done
+	cd /etc/lddl
+	lddl Vassar.lddl -server=$SERVER
 fi
 
 tail -f $LOG
