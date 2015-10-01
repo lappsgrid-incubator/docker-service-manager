a36b63b38c85f152ad5f524356428447904f15be
Keith Suderman <suderman@anc.org>
2015-10-01 15:17:40 -0400

    Added tar files to gitignore.

--- startup-vassar.sh
+++ startup-vassar.sh
@@ -9,10 +9,10 @@ until pg_isready &>/dev/null ; do
 done
 
 service tomcat start
+
 if [ -d /etc/lddl ] ; then
 	# Watch the log file for the startup complete message from Tomcat
 	tail -f $LOG | grep "INFO: Server startup in" | read -t 300 dummy_var
-	if [ $? -eq 0 ] ; then
 		cd /etc/lddl
 		lddl Vassar.lddl -server=$SERVER
 	else
