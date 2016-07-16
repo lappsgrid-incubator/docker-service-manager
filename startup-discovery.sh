3ac0eb8607c8664ca70c63bd8edb21665941cefe
Keith Suderman <suderman@cs.vassar.edu>
2016-07-16 09:49:05 -0400

    Updated Datasource version that is registered via LDDL.

--- startup-discovery.sh
+++ startup-discovery.sh
@@ -36,7 +36,7 @@ cd /etc/lddl
 git fetch origin
 git checkout docker
 lddl Vassar.lddl -server=$VASSAR
-lddl Update.lddl -server=$VASSAR -version=2.2.0 vassar/Datasources
+#lddl Update.lddl -server=$VASSAR -version=2.2.0 vassar/Datasources
 lddl Update.lddl -server=$BRANDEIS -version=2.0.1 brandeis/ServicesOpenNLP.lddl
 lddl Update.lddl -server=$BRANDEIS -version=2.0.1 brandeis/ServicesStanford.lddl
 lddl Update.lddl TempUser.lddl
